5f291762791c948b7d5123d84822ff226a7556fe
Andrew.Leo <andrew2007@foxmail.com>
2018-05-18 18:05:59 +0800

    andrew project init

--- android/app/src/main/java/com/meho/MainApplication.java
+++ android/app/src/main/java/com/meho/MainApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import com.RNFetchBlob.RNFetchBlobPackage;
-import com.oblador.vectoricons.VectorIconsPackage;
 import com.kmlidc.RNShareLocal.RNShareLocal;
 import com.theweflex.react.WeChatPackage;
 import io.realm.react.RealmReactPackage;
@@ -29,7 +28,6 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new RNFetchBlobPackage(),
-            new VectorIconsPackage(),
             new RNShareLocal(),
             new WeChatPackage(),
             new RealmReactPackage()
